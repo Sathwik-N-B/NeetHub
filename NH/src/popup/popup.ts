ab46d749574c619b0b829b9ce9ac332c2e47eaf0
Sathwik-N-B <sathwiknb1998@gmail.com>
2026-02-19 08:58:40 -0500

    updated github and website button

--- NH/src/popup/popup.ts
+++ NH/src/popup/popup.ts
@@ -18,6 +18,12 @@ init();
 async function init() {
   currentSettings = await chrome.runtime.sendMessage({ type: 'get-settings' }) as Settings;
 
+  // Website link opens the extension's welcome page
+  const websiteLink = document.getElementById('website-link') as HTMLAnchorElement;
+  if (websiteLink) {
+    websiteLink.href = chrome.runtime.getURL('src/welcome/welcome.html');
+  }
+
   updateUI();
 
   // Listen for settings changes
@@ -63,7 +69,7 @@ function updateUI() {
     repoLinkSection.style.display = 'none';
     statsSection.classList.remove('show');
 
-    authTitle.innerHTML = 'Set up your repository hook for <span style="color: #ff6b35;">NeetHub</span>';
+    authTitle.innerHTML = 'Set up your repository hook for <span style="color: #7B68B4;">NeetHub</span>';
     authBtn.innerHTML = `
       <svg class="github-icon" viewBox="0 0 24 24" fill="currentColor">
         <path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0024 12c0-6.63-5.37-12-12-12z"/>
@@ -78,7 +84,7 @@ function updateUI() {
     repoLinkSection.style.display = 'none';
     statsSection.classList.remove('show');
 
-    authTitle.innerHTML = 'Authenticate with GitHub to use <span style="color: #ff6b35;">NeetHub</span>';
+    authTitle.innerHTML = 'Authenticate with GitHub to use <span style="color: #7B68B4;">NeetHub</span>';
     authBtn.innerHTML = `
       <svg class="github-icon" viewBox="0 0 24 24" fill="currentColor">
         <path d="M12 0C5.37 0 0 5.37 0 12c0 5.31 3.435 9.795 8.205 11.385.6.105.825-.255.825-.57 0-.285-.015-1.23-.015-2.235-3.015.555-3.795-.735-4.035-1.41-.135-.345-.72-1.41-1.23-1.695-.42-.225-1.02-.78-.015-.795.945-.015 1.62.87 1.845 1.23 1.08 1.815 2.805 1.305 3.495.99.105-.78.42-1.305.765-1.605-2.67-.3-5.46-1.335-5.46-5.925 0-1.305.465-2.385 1.23-3.225-.12-.3-.54-1.53.12-3.18 0 0 1.005-.315 3.3 1.23.96-.27 1.98-.405 3-.405s2.04.135 3 .405c2.295-1.56 3.3-1.23 3.3-1.23.66 1.65.24 2.88.12 3.18.765.84 1.23 1.905 1.23 3.225 0 4.605-2.805 5.625-5.475 5.925.435.375.81 1.095.81 2.22 0 1.605-.015 2.895-.015 3.3 0 .315.225.69.825.57A12.02 12.02 0 0024 12c0-6.63-5.37-12-12-12z"/>
